48466380683116c8b5ee3c04e996d837dd4e78f7
SmailPang <smailpang@qq.com>
2026-05-19 15:44:30 +0800

    修改版本号为2.0.1

--- src/App.vue
+++ src/App.vue
@@ -1400,7 +1400,7 @@ onBeforeUnmount(() => {
                         </button>
                       </div>
                     </div>
-                <div class="toolbox-about-version" @click.stop>V{{ appVersion || '2.0.1-Beta.1' }}</div>
+                <div class="toolbox-about-version" @click.stop>V{{ appVersion || '2.0.1' }}</div>
                     <button class="toolbox-action-button toolbox-about-update" type="button" @click.stop="openUpdateDialog">{{ t('toolbox.checkUpdates') }}</button>
                   </div>
                 </Transition>
